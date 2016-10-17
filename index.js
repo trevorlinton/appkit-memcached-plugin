81b42c30baebb653b4966cc09fe3ef39caa84509
trevorlinton <trevor.linton@gmail.com>
2016-10-17 12:26:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function print_stats(appkit, args) {
+  console.assert(args.app, 'No app was specified.');
+  console.assert(args.ADDON, 'An addon id was not specified (see aka addons -a ' + args.app);
+  appkit.api.post(null, '/apps/' + args.app + '/addons/' + args.ADDON + '/actions/stats', appkit.terminal.print);
+}
+
+function flush_cache(appkit, args) {
+  console.assert(args.app, 'No app was specified.');
+  console.assert(args.ADDON, 'An addon id was not specified (see aka addons -a ' + args.app);
+  appkit.api.post(null, '/apps/' + args.app + '/addons/' + args.ADDON + '/actions/flush', appkit.terminal.print);
+}
+
+module.exports = {
+	init:function(appkit){
+    let create_build_option = {
+      'app':{
+        'alias':'a',
+        'demand':true,
+        'string':true,
+        'description':'The app memcached is installed on on.'
+      }
+    };
+    appkit.args.command('memcached <ADDON>','print out stats from the specified memcached', apps_options, print_stats);
+    appkit.args.command('memcached:flush <ADDON>','flushes the cache from the specified memcached', apps_options, flush_cache);
+	},
+	update:function(){},
+	group:'memcached',
+	help:'flush cache and get stats from memcached.',
+	primary:true
+};
